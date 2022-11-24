3ce144015717a879be890bd905be1a6905d3ef68
JDBIRLA <jitendra.birla21@gmail.com>
2022-11-24 16:21:18 +0530

    adding test repository project

--- Java_Guide/ws/spring-boot-testing/src/main/java/com/jd/springboot/repository/EmployeeRepository.java
+++ Java_Guide/ws/spring-boot-testing/src/main/java/com/jd/springboot/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.jd.springboot.repository;
 
 import com.jd.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by jd birla on 23-11-2022 at 09:49
@@ -9,4 +11,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    //JPQL query with index parameters
+    @Query("select e from Employee e where e.firstName =?1 and  e.lastName=?2")
+    Employee findByFirstLastName(String firstName , String lastName);
+
+    //JPQL query with named parameters
+    @Query("select e from Employee e where e.firstName =:firstName and  e.lastName=:lastName")
+    Employee findByNamedParams(@Param("firstName") String firstName , @Param("lastName") String lastName);
+
+    //Native sql query with index parameters
+    @Query(value = "select * from employees e where e.fisrt_name =?1 and  e.last_name=?2" , nativeQuery = true)
+    Employee findByNativeSql(String firstName , String lastName);
+
+    //Native sql query with named parameters
+    @Query(value = "select * from employees e where e.fisrt_name =:firstName and  e.last_name=:lastName" , nativeQuery = true)
+    Employee findByNativeSqlNamedParams(@Param("firstName") String firstName , @Param("lastName") String lastName);
 }
